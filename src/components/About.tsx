c43d6bc4ab2d0efe561f7ca0982ac2dd356cc779
Prolead1 <retroearthgeek1@gmail.com>
2022-09-29 00:14:16 +0800

    Checking deployment

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 
 const About: React.FunctionComponent = () => {
-  return <div className="bg-blue-300 w-full h-screen"></div>;
+  return <div className="bg-blue-300 w-full h-screen">Checking deployment frs...</div>;
 };
 
 export default About;
