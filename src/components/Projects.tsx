9cf462acf07a237475e444bf8129d698a9fae9b3
Prolead1 <retroearthgeek1@gmail.com>
2022-10-01 23:20:47 +0800

    remove title

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,12 +1,10 @@
 import * as React from 'react';
-import Title from './common/Title';
 import Terminal from './Terminal';
 
 const Projects: React.FunctionComponent = (props) => {
   return (
     <div className="bg-red-300 w-full h-screen">
       <div className="w-full h-screen pt-10">
-        <Title titleText="1. React Terminal" />
         <Terminal />
       </div>
     </div>
